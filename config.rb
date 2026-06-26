a6231e9288fcc833a4c8cd5d5fe382a5a82b43e0
Nick Burns <nburns@users.noreply.github.com>
2026-06-26 15:07:08 -0700

    Initial site scaffold

---
+++ config.rb
@@ -0,0 +1,15 @@
+activate :autoprefixer do |prefix|
+  prefix.browsers = "last 2 versions"
+end
+
+set :build_dir, 'docs'
+
+configure :development do
+  activate :livereload
+  activate :directory_indexes
+end
+
+configure :build do
+  set :relative_links, true
+  activate :relative_assets
+end
